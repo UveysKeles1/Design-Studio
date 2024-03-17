12893b86ce3c451c0759e4c4254996e120d70494
UveysKeles <s1081811@stud.sbg.ac.at>
2024-03-17 02:45:14 +0100

    hompage bearbeitet

--- one-way/src/app/app.module.ts
+++ one-way/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './home/nav-bar/nav-bar.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomepageComponent } from './home/homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomepageComponent],
   imports: [BrowserModule, MatToolbarModule, NavBarComponent],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
