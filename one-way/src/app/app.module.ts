74cba79052eec2496ed0d2cbd489cf8468d0d9d9
UveysKeles <s1081811@stud.sbg.ac.at>
2024-03-18 14:23:10 +0100

    bootstrap and sites

--- one-way/src/app/app.module.ts
+++ one-way/src/app/app.module.ts
@@ -6,9 +6,14 @@ import { NavBarComponent } from './home/nav-bar/nav-bar.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomepageComponent } from './home/homepage/homepage.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { SpeisekarteComponent } from './home/speisekarte/speisekarte.component';
+import { OpeningHoursComponent } from './home/opening-hours/opening-hours.component';
+import { KontaktComponent } from './home/kontakt/kontakt.component';
+import { AnfahrtComponent } from './home/anfahrt/anfahrt.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, FooterComponent, SpeisekarteComponent, OpeningHoursComponent, KontaktComponent, AnfahrtComponent],
   imports: [BrowserModule, MatToolbarModule, NavBarComponent],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
---
+++ one-way/src/app/home/speisekarte/speisekarte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-speisekarte',
+  templateUrl: './speisekarte.component.html',
+  styleUrl: './speisekarte.component.css'
+})
+export class SpeisekarteComponent {
+
+}
